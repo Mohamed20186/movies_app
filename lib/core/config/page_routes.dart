4b886f18972a5b81b0ab260545ed02188b6c20c1
mohamed ahmed <m.ahmed20201863@gmail.com>
2024-09-09 21:46:39 +0300

    create the infrastructure of project using clean architecture and MVVM

---
+++ lib/core/config/page_routes.dart
@@ -0,0 +1,7 @@
+class PageRoutes {
+  static const String initial = '/';
+  static const String onboardingView = '/onboarding_view';
+  static const String logInView = '/login_view';
+  static const String signUpView = '/signup_view';
+  static const String homeView = '/home_view';
+}
---
+++ lib/core/resources/colors_palette.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class ColorsPalette {
+  static const Color dark = Color(0xff1F1D2B);
+  static const Color soft = Color(0xff252836);
+  static const Color blueAccent = Color(0xff12CDD9);
+  static const Color green = Color(0xff22B07D);
+  static const Color orange = Color(0xffFF8700);
+  static const Color red = Color(0xffFF7256);
+  // text colors
+  static const Color textBlack = Color(0xff171725);
+  static const Color textDarkGrey = Color(0xff696974);
+  static const Color textGrey = Color(0xff92929D);
+  static const Color textWhiteGrey = Color(0xffF1F1F5);
+  static const Color textWhite = Color(0xffFFFFFF);
+  static const Color textLineDark = Color(0xffEAEAEA);
+}
